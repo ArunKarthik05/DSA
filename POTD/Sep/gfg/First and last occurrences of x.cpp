ac1dfce0c257b0db68393d082235165b204849d4
Arun Karthik <akkarthikk2002@gmail.com>
2023-09-22 10:06:38 +0530

    Sep22

---
+++ POTD/Sep/gfg/First and last occurrences of x.cpp
@@ -0,0 +1,10 @@
+public:
+    vector<int> find(int arr[], int n , int x )
+    {
+        int low = lower_bound(arr, arr + n, x) - arr;
+        int high = upper_bound(arr, arr + n, x) - arr;
+        if (low > high - 1) 
+            return {-1, -1};
+        return {low, high - 1};
+    }
+}
